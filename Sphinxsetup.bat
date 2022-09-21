18834fdcc2cc36abea51fd605d7231ca49869b5a
TunaLobster <charliepjohnson@gmail.com>
2022-09-21 14:27:34 -0500

    Move python packages to requirements.txt

--- Sphinxsetup.bat
+++ Sphinxsetup.bat
@@ -1,20 +1,8 @@
 rem remove any existing packages that may cause conflicts
 pip uninstall -y sphinx lxml sphinx-rtd-theme sphinxcontrib-youtube beautifulsoup4
 
-rem Install sphinx
-pip install --upgrade sphinx==5.1.1 docutils==0.16
-
-rem lxml for parameter parsing:
-pip install --upgrade lxml
-
-rem Install sphinx theme from ArduPilot repository
-pip install --upgrade git+https://github.com/ArduPilot/sphinx_rtd_theme.git
-
-rem and a youtube plugin:
-pip install --upgrade git+https://github.com/ArduPilot/sphinxcontrib-youtube.git
-
-rem and a parser to use getting posts from Discourse (forum) and insert in FrontEnd
-pip install --upgrade beautifulsoup4
+rem Install required python packages
+pip install --user --upgrade -r requirements.txt
 
 echo "Setup completed successfully!"
 pause
